dde7954f05b1b0f064b11c050b56384125408f5f
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-11-30 20:01:40 +0100

    Improved regex and added decorator

--- angular-component-factory.js
+++ angular-component-factory.js
@@ -1,7 +1,7 @@
-'use strict';
+(function (angular, undefined) {
+    'use strict';
 
-angular.module('socklessJS.utils.componentFactory', [])
-    .factory('componentFactory', function() {
+    var componentFactory = function() {
         return {
             createComponent: function(componentName, constructor) {
 
@@ -9,11 +9,14 @@ angular.module('socklessJS.utils.componentFactory', [])
 
                 if(!constructor.template && constructor.template !== 'false')
                 {
-                    var componentSnakeName = componentName.replace(/[A-Z]/g, function (match) {
-                        return "-"+match.toLowerCase();
-                    });
+                    var componentSnakeName = componentName
+                        .replace(/(?:[A-Z]+)/g, function (match) { //camelCase -> snake-case
+                            return "-"+match.toLowerCase();
+                        })
+                        .replace(/^-/, ''); // CamelCase -> -snake-case -> sake-case
 
                     componentSnakeName = componentSnakeName.replace(/-component/,'');
+                    constructor.componentSnakeName = componentSnakeName;
                     constructor.templateUrl = constructor.templateUrl || componentViewPath + componentSnakeName + '/' + componentSnakeName + '.html';
                 };
 
@@ -33,4 +36,42 @@ angular.module('socklessJS.utils.componentFactory', [])
                 return constructor
             }
         }
-    });
+    };
+
+    var decorateModule = function (module) {
+
+        //We need to handle components that might be registered before angular has finished loading
+        var queue = [];
+        //This only pushes constructors to a queue, and when angular is ready it registers the directives
+        module.component = function (name, constructor) {
+            queue.push({name: name, constructor: constructor});
+        };
+
+        module.config(function ($compileProvider) {
+            var compileProvider = $compileProvider;
+
+            module.component = function (name, constructor) {
+                //Register decorated directives
+                compileProvider.directive( (name + 'Component'), function ($injector) {
+                    return componentFactory().createComponent(name, $injector.invoke(constructor || angular.noop) || {});
+                });
+                return module; //To allow chaining
+            };
+
+            //Registered queued components
+            angular.forEach(queue, function (component) {
+                module.component(component.name,component.constructor);
+            });
+        });
+
+    };
+
+    //Expose factory to angular
+    angular.module('socklessJS.utils.componentFactory', []).factory('componentFactory', componentFactory);
+
+    //Expose decorator globally
+    angular.componentFactory = {
+        moduleDecorator: decorateModule
+    }
+
+}(angular));
