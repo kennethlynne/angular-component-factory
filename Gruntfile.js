9fb773524ae813b997ccc126c1f2f5c952622656
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-07 20:43:01 +0100

    add missing bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-ngmin');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
